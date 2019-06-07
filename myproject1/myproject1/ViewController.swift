23c8b21569d9c2203565fa57526173936d04997a
minho <minho@nain.jp>
2019-06-07 20:22:04 +0900

    firstappscreate

---
+++ myproject1/myproject1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  myproject1
+//
+//  Created by 陳ミンホ on 2019/06/07.
+//  Copyright © 2019 陳ミンホ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func onButtonTap(_ sender: Any) {
+    }
+    @IBOutlet weak var myTextFeild: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
